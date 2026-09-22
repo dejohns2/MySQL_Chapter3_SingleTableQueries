08574d6891ee0a46650d65341563b294f8dc8913
Debbie Johnson <62946732+dejohns2@users.noreply.github.com>
2026-09-22 10:09:41 -0500

    Debbie just finished query 2
    Added queries to report inactive customers and invalid email addresses.

--- MySQL_Chapter3.sql
+++ MySQL_Chapter3.sql
@@ -5,3 +5,32 @@ Chapter 3's Assignment
 GitHub URL: 
 ***********************************************/
 
+/****************************************************************************************************
+Debbie Johnson
+Query 1: Write a query that reports the following:
+•	inactive customers 
+•	sorted by name (NOTE: if not stated, all order by clauses are the default ascending)
+•	Note: to thoroughly test the query, 
+          temporarily change the WHERE clause to report active customers
+*****************************************************************************************************/
+
+SELECT 
+    customer_id,
+    CONCAT(last_name, ', ', first_name) AS cust_name,
+    email,
+    active
+FROM customer
+WHERE active = 0
+ORDER BY last_name , first_name;
+
+/****************************************************************************************************
+Debbie Johnson
+Query 2: Write a query that reports the following:
+•	All customers with INVALID email addresses. A valid email address is the first name, 
+	a period, the last name, and @sakilacustomer.org. Example: debbie.johnson@sakilacustomer.org
+•	Note: to thoroughly test this query, temporarily change the WHERE clause to report VALID emails
+*****************************************************************************************************/
+
+SELECT customer_id, first_name, last_name, email, active, create_date
+FROM customer
+WHERE email != CONCAT(first_name, '.', last_name, '@sakilacustomer.org');
